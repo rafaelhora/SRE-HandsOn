a6c0e9b3e9704e892b89f9582f72b6f1aa600e90
Rafael Fraga <f.rafaelhora@gmail.com>
2017-07-02 22:07:00 -0300

    Basic front end added
    Added basic homepage, its just a shell no interactions yet

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-#Basic Flask app just for testing
+#This program receives a string with a URL, the SLO for successful responses (double) and its the SLO for response time (double)
+#Makes a HTTP GET request every 5 seconds and calculates the current SLIs.
+#It displays for each URL the current SLIs, the SLOs and wheter the SLOs are being met
 
-from flask import Flask
+from flask import Flask, request, render_template #All Flask stuff
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/sup') #DEBUG page, serves to see if basic server cfgs are working
 def hello_world():
     return 'Hello inloco!'
+
+@app.route('/') #Main page, will get the URL and the desired SLOs
+def index():
+    return render_template('index.html')
+
+#This will render index.html for the user when localhost:5000/ is requested
+
+if __name__ == '__main__': #The script that is going to be executed
+    app.run(debug=True) #This facilitates development since you don't have to restart everything for every time you change your app, similar to nodejs

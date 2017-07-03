82e546a9dd5d5937900647697b297938399ad120
Rafael Fraga <f.rafaelhora@gmail.com>
2017-07-03 01:00:51 -0300

    Now the app can return a status for the request
    Now it prints a number equals to the status for the HTTP request

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 #Makes a HTTP GET request every 5 seconds and calculates the current SLIs.
 #It displays for each URL the current SLIs, the SLOs and wheter the SLOs are being met
 
-from flask import Flask, request, render_template #All Flask stuff
+import requests #Library that will handle the HTTP requests
+from flask import Flask, render_template     #All Flask stuff
+from flask import request
 
 app = Flask(__name__)
 
@@ -12,9 +14,13 @@ def hello_world():
 
 @app.route('/') #Main page, will get the URL and the desired SLOs
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #This will render index.html for the user when localhost:5000/ is requested
 
-#This will render index.html for the user when localhost:5000/ is requested
-
+@app.route('/sli') #Error: 500 internal error, can't seem to make the requests library to work
+def get_data():
+    #return 'hey man'
+    r = requests.get('https://api.github.com/events')
+    print r.status_code #returns the status of the request (200 for sucess)
+    return 'successful'
 if __name__ == '__main__': #The script that is going to be executed
     app.run(debug=True) #This facilitates development since you don't have to restart everything for every time you change your app, similar to nodejs
